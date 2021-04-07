61bbd4c73fc915dff7e7dba9faa8875c69609644
Goutiere <romain.goutiere@hds.utc.fr>
2021-04-07 18:55:37 +0200

    Creating an array

--- Text101/Text101/Assets/AdventureGame.cs
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingText;
+
+    State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("I am addrd programmatically !");
+        state = startingText;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
